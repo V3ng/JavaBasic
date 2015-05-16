46d520dc3b4accb5ee9ddf07f06f4fd0af4f6500
Антон <@Anton>
2015-05-16 03:03:21 +0300

    HT11 added

---
+++ src/com/sourceit/java/basic/gutorov/hometask11/TextStatisticsNew.java
@@ -0,0 +1,112 @@
+package com.sourceit.java.basic.gutorov.hometask11;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TextStatisticsNew extends TextStatistics{
+
+	public TextStatisticsNew(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main (String [] args){
+		TextStatisticsNew text = new TextStatisticsNew("Buddy you're a boy make a big noise Playin' in the street gonna be a big man some day You got mud on yo' face ");
+	
+		char letter = 'b';
+		System.out.println(text.getCurrentText());
+		System.out.println("Frequency of " + letter + " " + text.getCharFrequency(letter) + "%");  
+		System.out.println("Char count of "+ letter + " " + text.getCharCount(letter));
+		System.out.println(text.words());
+	}
+	
+	
+	private String getCurrentText() {
+				String result = getText().toLowerCase();
+				result.replaceAll("^[a-z]+", "");
+				return result;
+			}
+
+	@Override
+	public int getCharFrequency(char c) {
+		char [] arrOfChars = getCurrentText().toCharArray();
+		//System.out.println(arrOfChars.length);
+		int counter =0;         // Counter of char
+		int freq = 0;               // Frequency of char
+		for (int i = 0; i < arrOfChars.length; ++i){
+			
+			if (arrOfChars[i] == c) {
+				++counter;
+			}
+		}
+		//System.out.println(counter);
+		freq = (counter*100/arrOfChars.length);
+		return freq;
+	}
+
+	@Override
+	public int getCharCount(char c) {
+		char [] arrOfChars = getCurrentText().toCharArray();
+		int counter =0;
+		for (int i = 0; i < arrOfChars.length; ++i){
+			if (arrOfChars[i] == c) {
+				++counter;
+			}
+		}
+		return counter;
+	}
+
+	@Override
+	public Map<String, Integer> getChars() {
+		Map <Character, Integer> map = new TreeMap <> ();
+		char [] arrOfChars = getCurrentText().toCharArray();
+		for (int i = 0; i < arrOfChars.length; ++i){
+			char c = arrOfChars[i];
+			if (map.containsKey(c)){
+				map.put(c, map.get(c)+1);
+			}
+			map.put (c, 1);
+		}
+		return map ;          //Can`t correctly return this map 
+	}
+
+	@Override
+	public List<String> words() {
+		String t = null;
+		char [] arrOfChars = getText().toCharArray();
+		ArrayList <String> myList = new ArrayList<String>();
+		for (int i = 0; i < arrOfChars.length; ++i){
+			char c = arrOfChars[i];
+
+			while (c != ' ' ){
+				t = (t + c);}
+			myList.add(t);
+			System.out.println(t);
+			t = null;
+			
+
+		}
+		return myList;
+	}
+
+	@Override
+	public int getMostPopularCharCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLeastPopularCharCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public char getCharByCount(int count) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

526632e2b5bec9c4af9c48fa85f9e4fc1ca40dac
Антон <@Anton>
2015-05-19 01:56:47 +0300

    CW8 added

---
+++ src/com/sourceit/java/basic/gutorov/classwork8/GitTest.java
@@ -0,0 +1,14 @@
+package com.sourceit.java.basic.gutorov.classwork8;
+
+public class GitTest {
+
+	 public static void main (String [] args) {
+	 
+	 int a = 1;
+	 int b = 2;
+	 int c = 32;
+	 System.out.println (a + b + c);
+}
+}
+
+
